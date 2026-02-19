b8dd6eba35a703a4ca3e2fc193d94b9b4ab7b563
kseraks6 <kseraks60@example.com>
2026-02-19 10:08:32 +0100

    Add images and buton for modal

---
+++ js/modal.js
@@ -0,0 +1,8 @@
+const backdrop = document.querySelector('.backdrop');
+const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnClose = document.querySelector('.modal-btn-close');
+
+const toggleModal = () => backdrop.classList.toggle('is-hidden');
+
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);
